85db2f0859f9548aa5d1aa1399ec79e42dff3f48
Nishi Panchal <nishipanchal97@gmail.com>
2022-09-22 16:04:45 +0530

    new firebase app, create feature

--- firebase-config.js
+++ firebase-config.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDvLZ-Sfbozl-2IsS6wxn3NFwpcLUkhMj8",
-    authDomain: "company-list-app-61c64.firebaseapp.com",
-    projectId: "company-list-app-61c64",
-    storageBucket: "company-list-app-61c64.appspot.com",
-    messagingSenderId: "777334506322",
-    appId: "1:777334506322:web:5a475d6fb714877eb1f08b",
-    measurementId: "G-6PJPW7KL8J"
-  };
+  apiKey: "AIzaSyCX0xBSnidwmSyD9-ZPPmAsky_bgz60rXU",
+  authDomain: "company-list-app-e46a1.firebaseapp.com",
+  projectId: "company-list-app-e46a1",
+  storageBucket: "company-list-app-e46a1.appspot.com",
+  messagingSenderId: "598572399044",
+  appId: "1:598572399044:web:00a483eaa580d5fa478c91",
+  measurementId: "G-0F06YVXTJ8"
+};
 
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-  export const db = getFirestore(app);
+export const db = getFirestore(app);
--- pages/companies.js
+++ pages/companies.js
@@ -3,14 +3,24 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { db } from 'firebase-config';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import '../next.config';
 
 function Companies() {
-  const [items, setItems] = useState([]);
-  const itemsCollectionRef = collection(db, "company-list")
-  useEffect(() => {
+  const [newTitle, setNewTitle] = useState("")
+  const [newDescription, setNewDescription] = useState("")
+  const [newWebsite, setNewWebsite] = useState("")
+  const [newLogo, setNewLogo] = useState("")
 
+
+  const [items, setItems] = useState([]);
+  const itemsCollectionRef = collection(db, "company-list");
+
+  const createItem = async () => {
+    await addDoc(itemsCollectionRef, {title: newTitle, description: newDescription, url: newWebsite, logo: newLogo})
+  }
+
+  useEffect(() => {
     const getItems = async () => {
         const data = await getDocs(itemsCollectionRef);
         setItems(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
@@ -20,8 +30,17 @@ function Companies() {
   }, [itemsCollectionRef]);
 
   return (
-    <div>
-     
+    <div className="companies">
+          <div className="companies-input">
+            <input placeholder="Company Name" onChange={(event) => {setNewTitle(event.target.value)}}></input>
+            <input placeholder="Description" onChange={(event) => {setNewDescription(event.target.value)}}></input>
+            <input placeholder="Website Link" onChange={(event) => {setNewWebsite(event.target.value)}}></input>
+            <input placeholder="Logo Image URL" onChange={(event) => {setNewLogo(event.target.value)}}></input>
+            <button onClick={createItem}>Add Company</button>
+          </div>
+
+
+
           {items.map((item) => {
             return (
             // eslint-disable-next-line react/jsx-key
